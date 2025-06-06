55ce01f8de4cf3acd6daa55a076137e58af8a03a
Jena-Eleena <eleenajena2002@gmail.com>
2025-06-06 12:43:59 +0530

    Add Day5 HTML and CSS files

---
+++ june/week1/Day5/script.js
@@ -0,0 +1,139 @@
+console.log("Welcome");
+
+// variable declaration
+// let a=1
+// let A=1
+// let a=2 syntax error
+// console.log(a)
+// console.log(A)
+// let _="Qlith";
+// console.log(_)
+// let $="Qlith";
+// console.log($)
+
+// let $name="xyz"
+// console.log($name)
+
+// let _name="yz"
+// console.log(_name)
+// //camel case
+// let nameOfStudent="Eleena";  
+// console.log(nameOfStudent)
+// let a=20;
+// console.log(a);
+// console.log(typeof a)
+
+// let name="Qlith";
+// console.log(name, "type is ", typeof name)
+
+// let isPass=true;
+// console.log(isPass)
+// console.log(typeof isPass)
+
+// let empty=undefined;
+// console.log(empty)
+// console.log(typeof empty)
+
+// let nullValue=null;
+// console.log(nullValue)
+// console.log(typeof nullValue)
+
+// let b;
+// console.log(b)
+
+// let c=30
+// console.log(c);
+// console.log(typeof c)
+// console.log(typeof typeof c)
+
+      //-----Variable Creation---
+// let d=30.5;
+// console.log(typeof d)
+//global declaration of the variable
+// var fullName="Qlith Innovation";
+// console.log(fullName)
+// {
+//     console.log(fullName)
+// }
+
+// function getName(){
+//     console.log(fullName)
+// }
+
+// getName()
+
+//Normal Block
+//This is my block level declaration of variable
+// {
+//     var a=40;
+//     console.log("block level declaration of variable",a)
+// }
+// console.log(a)
+
+// function getA(){
+//     console.log("Function level declaration",a)
+// }
+// getA()
+
+// function variableDeclare(){
+//     var r=50;
+//     console.log(r)
+// }
+// variableDeclare();
+ 
+// console.log(r);
+
+
+// This is the let variable declaration
+// let name="Innovation"
+// console.log(name)
+// {
+//     console.log(name)
+// }
+ 
+// function getName(){
+//     console.log(name)
+// }
+// getName()
+//block level declaration
+// {
+//     let a=30;
+//     console.log(a)
+// }
+// console.log("local access:",a)
+
+//function level declaration
+// function gatA(){
+//     console.log("Function level declaration",a)
+//     console.log(name)
+// }
+
+//*This is the const variable declaration
+
+//global variable
+// const name="xyz";
+// console.log(name);
+// {
+//     console.log(name)
+// }
+
+// function getName(){
+//     console.log(name)
+// }
+
+// {
+//     const a=70;
+//     console.log(a);
+// }
+// console.log(a)
+
+let a=60
+console.log(a)
+
+a=40;
+console.log(a)
+
+a="y"
+console.log(a)
+
+
